b453fe3b1cec979650f39dbe55ea6fa66b8f72f9
Philipe Lopes <philipe.lopes07@gmail.com>
2020-12-19 13:40:59 -0300

    add node-sass, bootstrap in app

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,3 +1,21 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 export default function Login() {
-  return <h1> Login Page</h1>;
+  return (
+    <Container fluid>
+      <Row>
+        <p>header</p>
+      </Row>
+
+      <Row>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+      </Row>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  );
 }
